12b8b1c603adf267f53ef0f1f7a0e03e089b9cee
Saúl Rivera <saulriveal@gmail.com>
2017-08-19 22:03:56 -0500

    Logging screen added

---
+++ socialApp/FancyField.swift
@@ -0,0 +1,29 @@
+//
+//  FancyField.swift
+//  socialApp
+//
+//  Created by Saul Rivera on 8/19/17.
+//  Copyright © 2017 Dymtech. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.borderWidth = 0.5
+        layer.cornerRadius = 2.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+
+}
---
+++ socialApp/RoundedButton.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedButton.swift
+//  socialApp
+//
+//  Created by Saul Rivera on 8/19/17.
+//  Copyright © 2017 Dymtech. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        
+        layer.cornerRadius = self.frame.width / 2;
+
+    }
+
+}
